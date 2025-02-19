ecdd1521767dc852f554ae685edd9d021204daf2
kanarath <gerard.montero.s@gmail.com>
2025-02-19 19:08:07 +0100

    changed most of SVG to PNG due to shadow and blur issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        russo: ['"Russo One"', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        NavBar: "0px 0px 20px rgba(0, 214, 245, 0.5)",
+        Button: "0px 0px 50px rgba(16, 239, 255, 0.5)",
+      },
     },
     plugins: [],
-  };
+  },
+};
